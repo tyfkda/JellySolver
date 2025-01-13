419c2baff43e0d20b856f079c6498fcfcbeeb716
tyfkda <tyfkda@gmail.com>
2025-01-13 20:54:43 +0900

    解けるかどうかの判定修正

--- ruby/jelly/solver.rb
+++ ruby/jelly/solver.rb
@@ -33,7 +33,7 @@ module Jelly
                 unless @quiet
                     $stderr.print "\rCheck=#{check_count}, left=#{que.size}\x1b[0K"
                 end
-                next if unsolvable?(stage)
+                next if !@no_prune && unsolvable?(stage)
                 if stage.solved?()
                     @check_count = check_count
                     moves = extract_moves(nodes, key)
@@ -106,17 +106,33 @@ module Jelly
         end
 
         def unsolvable?(stage)
-            return false if @constraints.nil?
+            constraints = @constraints
+            up = 0
+            unless stage.hiddens.nil?
+                constraints = @constraints.dup() unless @constraints.nil?
+                stage.hiddens.each do |hidden|
+                    up += 1 if hidden[:dy] <= 0
+                    next if !hidden[:jelly].nil? || hidden[:link]
+                    y, color, dy = hidden.values_at(:y, :color, :dy)
+                    y += dy
+                    next if y >= @maxy
+                    if constraints.nil? || !constraints.has_key?(color) || constraints[color] > y
+                        constraints = {} if constraints.nil?
+                        constraints[color] = y
+                    end
+                end
+            end
+            return false if constraints.nil?
 
             color_heights = Hash.new(0)
             stage.jellies.each do |jelly|
-                next if jelly.color == BLACK || jelly.locked || !@constraints.has_key?(jelly.color)
+                next if jelly.color == BLACK || jelly.locked || !constraints.has_key?(jelly.color)
                 color_heights[jelly.color] += jelly.shape.h
             end
 
             stage.jellies.each do |jelly|
-                next if jelly.color == BLACK || jelly.locked || !@constraints.has_key?(jelly.color)
-                return true if jelly.y > @constraints[jelly.color] + color_heights[jelly.color]
+                next if jelly.color == BLACK || jelly.locked || !constraints.has_key?(jelly.color)
+                return true if jelly.y - color_heights[jelly.color] - up >= constraints[jelly.color]
             end
             return false
         end
@@ -128,38 +144,51 @@ module Jelly
             width = stage.width
             maxy = height - 1
             while true
-                break unless stage.wall_lines[maxy] == (1 << width) - 1
-                maxy -= 1
-            end
-
-            up = 0
-            unless stage.hiddens.nil?
-                stage.hiddens.each do |hidden|
-                    up += 1 if hidden[:dy] < 0
+                wall_line = stage.wall_lines[maxy]
+                unless wall_line == (1 << width) - 1
+                    vacant = (1..(width - 2)).any? do |x|
+                        if (wall_line & (1 << x)) != 0
+                            false
+                        else
+                            jelly = stage.jellies.find {|it| it.occupy_position?(x, maxy)}
+                            jelly.nil? || !jelly.locked
+                        end
+                    end
+                    break if vacant
                 end
+                maxy -= 1
             end
 
             stage.jellies.sort do |a, b|
                 a.y != b.y ? a.y <=> b.y : a.x <=> b.x
             end.each do |jelly|
-                break if jelly.y >= maxy - 1
+                break if jelly.y >= maxy
                 next if jelly.color == BLACK || !jelly.locked
-                y = jelly.y + jelly.shape.h - 1 + up
+                y = jelly.y + jelly.shape.h
+
+                # 下面が壁に覆われてたら下からつなげられないので、その分を補正
+                walls = (0...jelly.shape.w).all? do |dx|
+                    stage.wall_lines[y] & (1 << (jelly.x + dx)) != 0
+                end
+                y -= 1 if walls
+
                 if !constraints.has_key?(jelly.color) || constraints[jelly.color] > y
                     constraints[jelly.color] = y
                 end
             end
 
             stage.hiddens&.each do |hidden|
-                next unless hidden[:jelly].nil?
+                next unless hidden[:jelly].nil? && hidden[:link]
                 y, color, dy = hidden.values_at(:y, :color, :dy)
-                y += dy + up
+                y += dy
+                next if y >= maxy
                 if !constraints.has_key?(color) || constraints[color] > y
-                    constraints[color] = y + 1  # 本当は、残りのゼリーの高さを考慮する必要がある
+                    constraints[color] = y
                 end
             end
 
             @constraints = constraints.size > 0 ? constraints : nil
+            @maxy = maxy
         end
     end
 end
